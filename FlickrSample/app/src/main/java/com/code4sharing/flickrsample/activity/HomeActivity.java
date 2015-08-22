01abec3a28e8f62351365d2c73c4dbbb1bcb82fa
keyur47831 <keyur.thakkar@hotmail.com>
2015-08-22 23:10:58 +1000

    Added MapFragment

--- FlickrSample/app/src/main/java/com/code4sharing/flickrsample/activity/HomeActivity.java
+++ FlickrSample/app/src/main/java/com/code4sharing/flickrsample/activity/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.code4sharing.flickrsample.activity;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,24 +11,32 @@ import android.widget.Toast;
 import com.code4sharing.flickrsample.R;
 import com.code4sharing.flickrsample.model.FlickrDataModel;
 import com.code4sharing.flickrsample.presenter.FlickrPresenter;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
     FlickrPresenter mFlickrPresenter;
     private Toolbar mToolbar;
+    private GoogleMap mGoogleMap;
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_home);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = (Toolbar) findViewById (R.id.toolbar);
+        setSupportActionBar (mToolbar);
+        getSupportActionBar ().setDisplayShowHomeEnabled (true);
+        mFlickrPresenter = new FlickrPresenter (mRequestFlickrData);
+        initMap ();
 
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled (true);
-        mFlickrPresenter=new FlickrPresenter (mRequestFlickrData);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
@@ -45,25 +54,38 @@ public class HomeActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.current_location) {
-            mFlickrPresenter.loadData();
+            mFlickrPresenter.loadData ();
             return true;
         }
 
         return super.onOptionsItemSelected (item);
     }
-    private FlickrPresenter.onFectFlickrDataRequest mRequestFlickrData = new FlickrPresenter.onFectFlickrDataRequest(){
+
+    private FlickrPresenter.onFectFlickrDataRequest mRequestFlickrData = new FlickrPresenter.onFectFlickrDataRequest () {
 
         @Override
-        public void  onRequestSucess(List<FlickrDataModel> NearByLocation) {
-            Toast.makeText (getApplicationContext (), "DataSize : "+NearByLocation.size (), Toast.LENGTH_SHORT).show();
+        public void onRequestSucess (List<FlickrDataModel> NearByLocation) {
+            Toast.makeText (getApplicationContext (), "DataSize : " + NearByLocation.size (), Toast.LENGTH_SHORT).show ();
 
 
         }
 
         @Override
-        public void onRequestError() {
-            Toast.makeText (getApplicationContext (), R.string.connection_error, Toast.LENGTH_SHORT).show();
+        public void onRequestError () {
+            Toast.makeText (getApplicationContext (), R.string.connection_error, Toast.LENGTH_SHORT).show ();
 
         }
     };
+    private void initMap() {
+        if (mGoogleMap == null) {
+            MapFragment mapFragment = (MapFragment) getFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mGoogleMap=map;
+
+    }
 }
